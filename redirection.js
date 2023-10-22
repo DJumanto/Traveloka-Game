232ef3ca3ca64139214cbe21dd3eb2062b2f3e14
Alfa Fakhrur <100863813+DJumanto@users.noreply.github.com>
2023-10-22 19:52:17 +0700

    Add process for login and register

---
+++ redirection.js
@@ -0,0 +1,7 @@
+document.getElementById("login-button").addEventListener("click", function() {
+    location = "/login.html";
+});
+
+document.getElementById("register-button").addEventListener("click", function() {
+    location = "/register.html";
+});
